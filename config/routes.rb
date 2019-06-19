e1b3d3c3aa49532c170dbfacf08c1fb807a48812
johnhaus <john.haus@outlook.com>
2019-06-19 18:54:36 -0300

    few different setups around controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root 'cms#index'
 
-  # get 'cms/index'
+  get 'cms/index'
+  get 'cms/hello'
+  get 'cms/other_hello'
+  get 'cms/lynda'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
