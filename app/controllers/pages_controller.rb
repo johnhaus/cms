688260d713c067302b81b80dcccff956470ab619
johnhaus <john.haus@outlook.com>
2019-07-01 20:17:17 -0300

    set up before_action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
 
   layout 'admin' # replaces application.html.erb
 
+  # Instead fo using   @subjects = Subject.sorted   in each of the actions
+  # new, create, edit, update, can set before_action. It does, however, create
+  # a dateabase call at start of create/update instead of later
+  before_action :find_subjects, :only => [:new, :create, :edit, :update]
+  before_action :set_page_count, :only => [:new, :create, :edit, :update]
+
   def index
     @pages = Page.sorted # will sort by position, can use Page.all as well
   end
@@ -12,26 +18,24 @@ class PagesController < ApplicationController
 
   def new
     @page = Page.new
-    @page_count = Page.count + 1
-    @subjects = Subject.sorted
+    # @page_count = Page.count + 1  used here thru :set_page_count with the += 1
   end
 
   def create
+    # @subjects = Subject.sorted    is called here with before_action
     @page = Page.new(page_params)
     if @page.save
       flash[:notice] = "Page created successfully"
       redirect_to(pages_path)
     else
       @page_count = Page.count + 1
-      @subjects = Subject.sorted
       render('new')
     end
   end
 
   def edit
     @page = Page.find(params[:id])
-    @page_count = Page.count
-    @subjects = Subject.sorted
+    # set_page_count  used here
   end
 
   def update
@@ -40,8 +44,6 @@ class PagesController < ApplicationController
       flash[:notice] = "Page updated successfully"
       redirect_to(pages_path(@page))
     else
-      @page_count = Page.count
-      @subjects = Subject.sorted
       render('edit')
     end
   end
@@ -61,6 +63,17 @@ class PagesController < ApplicationController
 
   def page_params
     params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+  end
+
+  def find_subjects
+    @subjects = Subject.sorted
+  end
+
+  def set_page_count
+    @page_count = Page.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @page_count += 1
+    end
   end
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -13,7 +13,7 @@ class Section < ApplicationRecord
 
   validates_presence_of :name
   validates_length_of :name, :maximum => 255
-  valdates_inclusion_of :content_type, :in => CONTENT_TYPES, :message => "must be one of: #{CONTENT_TYPES.join(', ')}"
+  validates_inclusion_of :content_type, :in => CONTENT_TYPES, :message => "must be one of: #{CONTENT_TYPES.join(', ')}"
   validates_presence_of :content
 
 end
