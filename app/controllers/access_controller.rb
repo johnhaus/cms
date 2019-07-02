441e86fc80abc7e09ab758f2bbb341d4bbc6a689
johnhaus <john.haus@outlook.com>
2019-07-02 15:33:18 -0300

    configured access controller

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -11,9 +11,28 @@ class AccessController < ApplicationController
   end
 
   def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = AdminUser.where(:username => params[:username]).first
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+
+    if authorized_user
+      session[:user_id] = authorized_user.id
+      flash[:notice] = "Logged in successfully."
+      redirect_to(admin_path)
+    else
+      flash.now[:notice] = "Invalid username/password." # Shows flash notice immediately
+      render('login')
+    end
+
   end
 
   def logout
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to(access_login_path)
   end
 
 end
